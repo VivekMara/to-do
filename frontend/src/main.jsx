aaaf41d64da6d331380bac2a4fd9d2757f23e1da
VivekMara <vivek.v.mara@gmail.com>
2024-06-17 11:27:29 +0530

    routing
    added some basic routing in react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AddTask from './AddTask.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import GetTask from './GetTask.jsx'
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<div>Hello World</div>
+    },
+    {
+      path:"addtask",
+      element:<AddTask/>
+    },
+    {
+      path:"gettask",
+      element:<GetTask/>
+    }
+  ]
+)
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
